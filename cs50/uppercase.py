77dbfa2a6edfdb2d04263e91d1c462b7f61bfa47
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-03 21:19:49 -0300

    Create uppercase.py

---
+++ cs50/uppercase.py
@@ -0,0 +1,12 @@
+#uppercase the letters
+before = input("Before: ")
+print(f"After: {before.upper()}")
+
+
+
+
+
+"""
+for c in before:
+    print(c.upper(), end="")        #uppercase each caracter
+"""
