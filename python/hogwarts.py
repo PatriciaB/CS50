120e253f36f0946d3cecd5d4fa73e5eee7366e6b
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-03 21:21:11 -0300

    Create hogwarts.py

---
+++ python/hogwarts.py
@@ -0,0 +1,9 @@
+students = ["Herminone", "Harry", "Ron"]                    #lista com 3 elementos
+
+"""
+for student in students:                #printar todos da lista, nao precisa inicializar
+    print(student)
+"""
+
+for i in range(len(students)):          # imprime a posicao len é o tamanho, range é o tamanho
+    print(i+1, students[i])
