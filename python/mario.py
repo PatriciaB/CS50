00c832f4403ea89be79c143649b492e64244d399
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-03 21:23:32 -0300

    Create mario.py

---
+++ python/mario.py
@@ -0,0 +1,10 @@
+def main():
+    print_column(3)
+
+
+def print_column(height):
+    for _ in range(height):
+       print("#")
+#    print("#\n" * height, end="")               #outra forma de representar
+
+main()
