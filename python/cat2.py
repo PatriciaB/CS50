6588234cb0ba8aa5b8ffb1f316d921eb7df130b7
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-03 21:20:56 -0300

    Create cat2.py

---
+++ python/cat2.py
@@ -0,0 +1,5 @@
+for _ in range(2):  #number of values      #_ nao cria variavel           out ex  for i in range(2):
+    print("meow")
+
+
+#print("Meow\n" * 3, end ="")   # outra forma de representar em python
