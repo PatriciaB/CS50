620a6a92cc818edc1f7cfd6be605933b4420c243
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-03 21:23:50 -0300

    Create students.py

---
+++ python/students.py
@@ -0,0 +1,10 @@
+students = {
+    "Hermine": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+    "Padma": "Ravenclaw",
+}  # dictionary
+
+for student in students:
+    print(student)
